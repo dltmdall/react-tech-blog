e72f96f5aadc8b28499cc2ecde21be8c71dcd354
Seung Mi Lee <107185525+dltmdall@users.noreply.github.com>
2026-08-27 18:52:56 +0900

    refactor: Header와 Profile 컴포넌트 분리

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <a href="/">승미의 기술 블로그</a>
+      <nav aria-label="주요 메뉴">
+        <ul>
+          <li><a href="#posts">글</a></li>
+          <li><a href="#about">소개</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
